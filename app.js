8a9fcfd9b2317fb6369264aefcaaf0f844238639
Jay Carpenter <jaycarpenter@desertblockchain.com>
2021-09-08 08:44:40 -0700

    test update add s to app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>My Node App</h1>');
+    res.send('<h1>My Node Apps</h1>');
 });
 
 app.listen(5000, () => {
